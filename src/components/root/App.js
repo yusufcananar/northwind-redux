54d4c0576ae6aa7fd85fea0968c65e4637f07031
Fjolnirr <yusufcan2617@gmail.com>
2022-12-19 14:27:53 +0300

    -Actions added. -Reducers added. -configStore added. -Component connect added.

--- src/components/root/App.js
+++ src/components/root/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Container } from "reactstrap";
+import Navi from "../navi/Navi";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
-    <div>
-      <h1>MY APP</h1>
-    </div>
+    <Container>
+      <Navi />
+      <Dashboard />
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/root/App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import configureStore from './redux/reducers/configureStore';
 
+const store=configureStore();
+// @ts-ignore
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
